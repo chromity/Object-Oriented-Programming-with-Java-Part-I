1417d3f4c1ad4bb0228401b323d280de99fe537c
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-09 20:57:50 +0800

    Added completed solution to Project 008 & 009
    Completed the exercises 008 & 009. From now project should be renamed as
    exercises. Mistake on my part.

--- week1-008.Adder/src/Adder.java
+++ week1-008.Adder/src/Adder.java
@@ -5,7 +5,17 @@ public class Adder {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        System.out.print("Type a number: ");
+        int a = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type another number: ");
+        int b = Integer.parseInt(reader.nextLine());
+        
+        int sum = a + b;
+        
+        System.out.println("\nSum of the numbers: " + sum);
+        
         // Implement your program here. Remember to ask the input from user
     }
 }
--- week1-009.Divider/src/Divider.java
+++ week1-009.Divider/src/Divider.java
@@ -5,7 +5,16 @@ public class Divider {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        System.out.print("Type a number: ");
+        int a = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type another number: ");
+        int b = Integer.parseInt(reader.nextLine());
+        
+        double quotient = 1.0 * a / b; // converts the product to a double
+        
+        System.out.print("\nDivision: " + a + " / " + b + " = " + quotient);
         // Implement your program here. Remember to ask the input from user.
     }
 }
