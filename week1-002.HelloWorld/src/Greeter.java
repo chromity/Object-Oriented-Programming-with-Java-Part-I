e541275dfa86f4c0c3a56168cf1c6485877a5e26
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-09 18:44:32 +0800

    Added completed solution to Project 002 & 003

--- week1-002.HelloWorld/src/Greeter.java
+++ week1-002.HelloWorld/src/Greeter.java
@@ -1,7 +1,8 @@
 public class Greeter {
 
     public static void main(String[] args) {
-        // Write your code here
+        System.out.println("Hello world!");
+        System.out.println("(And all the people of the world)");
 
     }
 
