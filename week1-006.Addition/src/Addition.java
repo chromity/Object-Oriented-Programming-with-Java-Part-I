ffbd10519e502d34ce937922063cec200b63c610
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-09 20:43:19 +0800

    Revert "Added completed solution to Project 006 & 007 and other project files"
    This reverts commit 905ad9f8468d619e2febf5cf6939abd22aaf2ea9.

--- week1-006.Addition/src/Addition.java
+++ week1-006.Addition/src/Addition.java
@@ -4,7 +4,7 @@ public class Addition {
 
         int a = 1337;
         int b = 42;
-        int result = a + b; // Fix this
+        int result = 0; // Fix this
 
         String toPrint = a + " + " + b + " = " + result;
         System.out.println(toPrint);
