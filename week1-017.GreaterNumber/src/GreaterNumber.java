62c9a74fe56e77fe0933050bb812dc21bc386dce
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-10 20:31:44 +0800

    Added completed solution to exercises 017

--- week1-017.GreaterNumber/src/GreaterNumber.java
+++ week1-017.GreaterNumber/src/GreaterNumber.java
@@ -4,6 +4,19 @@ public class GreaterNumber {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        System.out.print("Type the first number: ");
+        int a = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type the second number: ");
+        int b = Integer.parseInt(reader.nextLine());
+        
+        if (a > b) {
+            System.out.println("Greater number: " + a);
+        } else if (b > a) {
+            System.out.println("Greater number: " + b);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
     }
 }
