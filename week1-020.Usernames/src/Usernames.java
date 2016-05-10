623da922d8a122f1d996964a19532750b79291b5
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-10 21:01:22 +0800

    Added completed solution to exercises 019 & 020

--- week1-020.Usernames/src/Usernames.java
+++ week1-020.Usernames/src/Usernames.java
@@ -6,5 +6,26 @@ public class Usernames {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type your username: ");
+        String username = reader.nextLine();
+
+        System.out.print("Type your password: ");
+        String password = reader.nextLine();
+
+        if (username.equals("alex")) {
+            if (password.equals("mightyducks")) {
+                System.out.println("You are now logged into the system!");
+            } else {
+                System.out.println("Your username or password was invalid!");
+            }
+        } else if (username.equals("emily")) {
+            if (password.equals("cat")) {
+                System.out.println("You are now logged into the system!");
+            } else {
+                System.out.println("Your username or password was invalid!");
+            }
+        } else {
+            System.out.println("Your username or password was invalid!");
+        }
     }
 }
