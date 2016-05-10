1047d29e4cc703e139de3a417221008157573a26
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-10 22:07:35 +0800

    Added completed solution to exercises 022-024. Ended week 1.

--- week1-022.Password/src/Password.java
+++ week1-022.Password/src/Password.java
@@ -7,6 +7,17 @@ public class Password {
         Scanner reader = new Scanner(System.in);
         String password = "carrot"; // Use carrot as password when running tests.
 
-        // Write your code here
+        while (true) {
+            System.out.print("Type the password: ");
+            String pw = reader.nextLine();
+            
+            if (pw.equals(password)) {
+                System.out.println("Right!");
+                System.out.println("The secret is: jryy qbar!");
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
     }
 }
--- week1-023.Temperatures/src/Temperatures.java
+++ week1-023.Temperatures/src/Temperatures.java
@@ -6,14 +6,13 @@ public class Temperatures {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        // Write your code here. 
+        while (true) {
+            System.out.print("Type a float number to add to the graph: ");
+            double temp = Double.parseDouble(reader.nextLine());
 
-        // Graph is used as follows:
-        Graph.addNumber(7);
-        double value = 13.5;
-        Graph.addNumber(value);
-        value = 3;
-        Graph.addNumber(value);
-        // Remove or comment out these lines above before trying to run the tests.
+            if (temp >= -30 && temp <= 40) {
+                Graph.addNumber(temp);
+            }
+        }
     }
 }
