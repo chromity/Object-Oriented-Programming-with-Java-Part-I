82d31e907db313f15ce4d93937ee16de4400bd8a
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-11 22:39:13 +0800

    Added completed solution to exercises 035-036

--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -6,5 +6,16 @@ public class SumOfThePowers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        
+        int sum = 0;
+        int power = 0;
+        while (power <= number) {
+            sum += (int)Math.pow(2, power);
+            power++;
+        }
+        
+        System.out.println("The result is " + sum); 
     }
 }
--- week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
+++ week2-036.LoopsEndingRemembering/src/LoopsEndingRemembering.java
@@ -6,6 +6,37 @@ public class LoopsEndingRemembering {
         // actually this is just one program that is split in many parts
         
         Scanner reader = new Scanner(System.in);
-
+        
+        int sum = 0;
+        int counter = 0;
+        int even = 0;
+        int odd = 0;
+        System.out.println("Type numbers:");
+        while (true) {
+            int number = Integer.parseInt(reader.nextLine());
+            
+            if (number == -1) {
+                System.out.println("Thank you and see you later!");
+                break;
+            }
+            
+            sum += number;
+            counter++;
+            
+            if (number % 2 == 0) {
+                even += 1;
+            } else {
+                odd += 1;
+            }
+        }
+        
+        System.out.println("The sum is " + sum);
+        System.out.println("How many numbers: " + counter);
+        
+        double average = 1.0 * sum / counter;
+        System.out.println("Average: " + average);
+        
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
     }
 }
