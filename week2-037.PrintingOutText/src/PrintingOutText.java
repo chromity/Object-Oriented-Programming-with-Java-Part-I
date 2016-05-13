6a3af98d12a355d94d15c120123a159fb9c6520a
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-13 13:49:54 +0800

    Added completed solution for exercises 037 & 038

--- week2-037.PrintingOutText/src/PrintingOutText.java
+++ week2-037.PrintingOutText/src/PrintingOutText.java
@@ -1,7 +1,8 @@
 public class PrintingOutText {
 
     public static void printText() {
-        // Write your code here
+        System.out.println("In the beginning there were the swamp, the hoe"
+                + " and Java.");
     }
 
     public static void main(String[] args) {
