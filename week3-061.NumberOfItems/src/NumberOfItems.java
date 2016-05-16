5417c5bd1219517cd30b359e1a9bf8833fc150ab
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-05-16 11:47:22 +0800

    Added completed solution to exercises 061 & 062

--- week3-061.NumberOfItems/src/NumberOfItems.java
+++ week3-061.NumberOfItems/src/NumberOfItems.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 
 public class NumberOfItems {
 
-    // implement here the method countItems
+    public static int countItems(ArrayList<String> items) {
+        return items.size();
+    }
 
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
@@ -10,8 +12,7 @@ public class NumberOfItems {
         list.add("Ciao");
         list.add("Hello");
         System.out.println("There are this many items on the list:");
-        // You can remove the comment from below when the method is done
-        //System.out.println(countItems(list)); 
+        System.out.println(countItems(list)); 
     }
 
 }
