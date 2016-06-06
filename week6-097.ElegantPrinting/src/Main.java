5927a22706209ad62239871bb13b62342a3e29c3
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-06-06 21:46:28 +0800

    Added solution to exercise 97

--- week6-097.ElegantPrinting/src/Main.java
+++ week6-097.ElegantPrinting/src/Main.java
@@ -8,6 +8,12 @@ public class Main {
     }
 
     public static void printElegantly(int[] array) {
-        // Write code here
+        
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        
+        System.out.println(array[array.length - 1]);
+        
     }
 }
