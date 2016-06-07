3d9d58651a906acbf06359c765b037c53128b0cd
Michael Daryl Mayo <michaeldaryl.mayo@gmail.com>
2016-06-07 15:17:02 +0800

    Added solution to exercise 102
    The code was a little messy, will try hard on the next exercise

--- week6-102.GradeDistribution/src/Main.java
+++ week6-102.GradeDistribution/src/Main.java
@@ -1,12 +1,96 @@
+
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);
-        // implement your program here
-        // do not put all to one method/class but rather design a proper structure to your program
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Integer> scores = new ArrayList<Integer>();
+
+        askScore(reader, scores);
+    }
+
+    public static void askScore(Scanner reader, ArrayList<Integer> scoreList) {
+        int input = 0;
+        while (true) {
+            input = Integer.parseInt(reader.nextLine());
+            if (input != -1) {
+                scoreList.add(input);
+            } else {
+                break;
+            }
+        }
+
+        sortScore(scoreList);
+    }
+
+    public static void sortScore(ArrayList<Integer> scoreList) {
+        int countOfFailed = 0;
+        int size = 0; // size of scoreList
         
-        // Your program should use only one Scanner object, i.e., it is allowed to call 
-        // new Scanner only once. If you need scanner in multiple places, you can pass it as parameter
+        for (int score : scoreList) {
+            if (score >= 0 && score <= 60) {
+                size++;
+            }
+        }
+        
+        System.out.println("Grades distribution:");
+
+        System.out.print("5: ");
+        for (int score : scoreList) {
+            if (score >= 50 && score <= 60) {
+                System.out.print("*");
+            }
+        }
+        System.out.println("");
+
+        System.out.print("4: ");
+        for (int score : scoreList) {
+            if (score >= 45 && score < 50) {
+                System.out.print("*");
+            }
+        }
+        System.out.println("");
+
+        System.out.print("3: ");
+        for (int score : scoreList) {
+            if (score >= 40 && score < 45) {
+                System.out.print("*");
+            }
+        }
+        System.out.println("");
+
+        System.out.print("2: ");
+        for (int score : scoreList) {
+            if (score >= 35 && score < 40) {
+                System.out.print("*");
+            }
+        }
+        System.out.println("");
+
+        System.out.print("1: ");
+        for (int score : scoreList) {
+            if (score >= 30 & score < 35) {
+                System.out.print("*");
+            }
+        }
+        System.out.println("");
+
+        System.out.print("0: ");
+        for (int score : scoreList) {
+            if (score < 30 && score >= 0) {
+                System.out.print("*");
+                countOfFailed++;
+            }
+        }
+        System.out.println("");
+
+        if (scoreList.size() != 0) {
+            System.out.println("Acceptance percentage: " + (double) ((size
+                    - countOfFailed)) * 100 / size);
+        } else {
+            System.out.println("Acceptance percentage: 0");
+        }
     }
 }
